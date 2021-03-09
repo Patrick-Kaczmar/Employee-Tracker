cc17b041351bf4072ff3f26ed8d7401738de9848
Zhylow <pk1blue@yahoo.com>
2021-03-08 22:36:11 -0500

    added exit function for program

--- server.js
+++ server.js
@@ -11,5 +11,40 @@ const connection = mysql.createConnection({
 
 connection.connect( (err) => {
     if (err) throw err;
-    // console.log('u r connect yep')
-})
+    firstQuestion();
+});
+
+function firstQuestion() {
+    inquirer.prompt(
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Add new Data", "Update roles", "View data", "Delete data", "Exit"],
+            name: "firstChoice"
+
+        }
+    ).then(response => {
+        const { answer } = response;
+        
+        if (answer === "Add new Data") {
+            addNewData();
+        }
+        else if (answer === "Update roles") {
+            updateRoles();
+        }
+        else if (answer === "View data") {
+            viewData();
+        }
+        else if (answer === "Delete data") {
+            deleteData();
+        }
+        else {
+            exit();
+        }
+    });
+}
+
+function exit() {
+    connection.end();
+    console.log("You have exited the program")
+}
